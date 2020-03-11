1869313eb4f3ad701f79125f2458f01692f80f0a
Kunal Singhal <kunal.singhal@grammarly.com>
2020-03-10 17:46:54 -0700

    feature: command to create a new branch - lame I know!

--- startup.sh
+++ startup.sh
@@ -5,7 +5,7 @@ alias zz="source $HOME/startup.sh"
 # git aliases
 alias clean="git branch | grep -v "master" | xargs git branch -D" # remove all branches apart from master
 alias ok="git add --all && git commit --amend --no-edit"
+alias feature"git checkout -b"
 
 # cleanup files
 alias remove="rm *~ &#"
-
