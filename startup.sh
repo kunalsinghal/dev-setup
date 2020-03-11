dc2e1176b67bb0c8bf4f1814363befdb377e8990
Kunal Singhal <kunal.singhal@grammarly.com>
2020-03-10 17:37:31 -0700

    add remove

--- startup.sh
+++ startup.sh
@@ -1,6 +1,11 @@
+# self serving aliases
 alias zr="emacs $HOME/startup.sh && source $HOME/startup.sh"
+alias zz="source $HOME/startup.sh"
 
-# git commands
+# git aliases
 alias clean="git branch | grep -v "master" | xargs git branch -D" # remove all branches apart from master
 alias ok="git add --all && git commit --amend --no-edit"
+
+# cleanup files
+alias remove="rm *~ &#"
 
