b5b9d9fce77f93bc1eee745fcb268a4e1db26954
Kunal Singhal <kunal.singhal@grammarly.com>
2022-04-28 23:50:32 -0700

    update

--- startup.sh
+++ startup.sh
@@ -3,16 +3,20 @@ alias zr="emacs $HOME/startup.sh && source $HOME/startup.sh && echo WARNING: the
 alias zz="source $HOME/startup.sh"
 
 # git aliases
-alias clean="git branch | grep -v "master" | xargs git branch -D" # remove all branches apart from master
+alias clean="git branch | grep -v "master" | grep -v "main" | xargs git branch -D" # remove all branches apart from master
 alias ok="git add --all && git commit --amend --no-edit"
 alias feature"git checkout -b"
 alias branch="git rev-parse --abbrev-ref HEAD | tr -d '\n'"
 alias gcaa="(git rev-parse --abbrev-ref HEAD | tr -d '\n' | pbcopy); git commit -v -a"
+alias gpn="git push --no-verify"
 function where(){
     git show --pretty="" --name-only $1 | cat
 }
+alias reset="git reset --hard"
 
 # cleanup files
 alias remove="rm *~ &#"
+
+alias code="open -a Visual\ Studio\ Code"
 
 export PATH=$PATH:~/.local/bin
